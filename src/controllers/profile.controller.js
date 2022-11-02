b9f2b94864f700f72c91d0655d051a05783eb4af
JeanVittory <vittory.dev@gmail.com>
2022-11-02 17:23:46 -0500

    analisis de carga

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -2,7 +2,7 @@ import { app } from '../config/app.config.js';
 import { logger } from '../config/logger/index.js';
 
 const getProfile = (req, res) => {
-  logger.info(`accessing the route: ${req.path}`);
+  logger.info(`accessing the route: ${req.baseUrl}`);
   if (req.isAuthenticated()) {
     const username = req.params.username;
     res.render('main', { layout: 'index', username: username });
@@ -13,13 +13,13 @@ const getProfile = (req, res) => {
 };
 
 const auth = (req, res) => {
-  logger.info(`accessing the route: ${req.path}`);
+  logger.info(`accessing the route: ${req.baseUrl}`);
   const { username } = req.body;
   res.redirect(`http://localhost:${app.get('port')}/api/v1/profile/${username}`);
 };
 
 const logout = (req, res) => {
-  logger.info(`accessing the route: ${req.path}`);
+  logger.info(`accessing the route: ${req.baseUrl}`);
   req.logout((err) => {
     if (err) {
       logger.error(err);
